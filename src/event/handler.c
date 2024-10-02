149143ea7649a1e5e13c9d2792785bd026138dae
pablo <pajujica@gmail.com>
2024-10-02 19:30:44 +0200

    events optimised

--- src/event/handler.c
+++ src/event/handler.c
@@ -6,7 +6,7 @@
 /*   By: pajimene <pajimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:12:22 by pajimene          #+#    #+#             */
-/*   Updated: 2024/10/02 19:01:42 by pajimene         ###   ########.fr       */
+/*   Updated: 2024/10/02 19:30:11 by pajimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,30 @@ static void ft_rotate(t_player *p, double angle)
 	p->plane.y = tmp_x * sin(angle) + p->plane.y * cos(angle);
 }
 
+void	ft_move_up(t_player *p)
+{
+	p->pos.x += p->dir.x * 0.1;
+	p->pos.y += p->dir.y * 0.1;
+}
+
+void	ft_move_down(t_player *p)
+{
+	p->pos.x -= p->dir.x * 0.1;
+	p->pos.y -= p->dir.y * 0.1;
+}
+
+void	ft_move_left(t_player *p)
+{
+	p->pos.x -= p->plane.x * 0.1;
+	p->pos.y -= p->plane.y * 0.1;
+}
+
+void	ft_move_right(t_player *p)
+{
+	p->pos.x += p->plane.x * 0.1;
+	p->pos.y += p->plane.y * 0.1;
+}
+
 int ft_key(int keysym, t_data *data)
 {
 	t_player *p;
@@ -32,25 +56,13 @@ int ft_key(int keysym, t_data *data)
 	if (keysym == XK_Escape)
 		ft_close(data);
 	if (keysym == XK_w)
-	{
-		p->pos.x += p->dir.x * 0.1;
-		p->pos.y += p->dir.y * 0.1;
-	}
+		ft_move_up(p);
 	else if (keysym == XK_s)
-	{
-		p->pos.x -= p->dir.x * 0.1;
-		p->pos.y -= p->dir.y * 0.1;
-	}
+		ft_move_down(p);
 	else if (keysym == XK_d)
-	{
-		p->pos.x += p->plane.x * 0.1;
-		p->pos.y += p->plane.y * 0.1;
-	}
+		ft_move_right(p);
 	else if (keysym == XK_a)
-	{
-		p->pos.x -= p->plane.x * 0.1;
-		p->pos.y -= p->plane.y * 0.1;
-	}
+		ft_move_left(p);
 	else if (keysym == XK_Right)
 		ft_rotate(p, p->angle);
 	else if (keysym == XK_Left)
